1292eccb89be0ff237a6a2068916f902ddc2b817
GoranKim0518 <109512293+GoranKim0518@users.noreply.github.com>
2022-10-26 21:46:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {OrbitControls} from "/OrbitControls.js";
+import {OrbitControls} from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer( {antialias: true});
 const render_w = 640;
